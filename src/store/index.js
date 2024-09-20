f984f16807e4e246d7fecae765fc7169466bb3e0
cd4761 <cd4761@gmail.com>
2024-09-20 18:17:58 +0900

    update context

--- src/store/index.js
+++ src/store/index.js
@@ -880,8 +880,8 @@ dividendPool: ${onChainEffects[1].values[3]}
         ) {
           return `Currently, TON seigniorage is issued each time a Ethereum block is created.
 
-Additionally issued TON will be distributed among PowerTON, DAO and staking users, excluding TON allocated for fixed seignorage rewards (19%).
-This function allows you to determine the ratio of the newly issued TON accumulated for PowerTON, DAO and staking users.`;
+3.92 TON is issued as seigniorage with each block and distributed among PowerTON, DAO, and staking users. This amount stems from 19% of the initial TON supply of 50,000,000 TON, converted into a fixed annual seigniorage supply.
+This function lets you set the distribution ratio of the 3.92 TON among PowerTON, DAO, and staking users.`;
         }
       }
       if (!onChainEffects || onChainEffects.length === 0) {
